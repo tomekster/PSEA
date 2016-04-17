8ea5be0a29f696cb89bdb3defd3a457b0a595d0d
Tomasz Sternal <sternal.tomasz@gmail.com>
2016-04-17 23:30:04 +0200

    Add DTLZ4

---
+++ src/problems/DTLZ4.java
@@ -0,0 +1,71 @@
+package problems;
+
+import core.Problem;
+import core.Solution;
+
+public class DTLZ4 extends Problem {
+	// Parameter used in Deb's original paper
+	// k = numVariables - numObjectives + 1
+	private final static int k = 10;
+
+	public DTLZ4(Integer numObjectives) {
+		this(numObjectives + k - 1, numObjectives);
+	}
+
+	public DTLZ4(int numVariables, int numObjectives) {
+		super(numVariables, numObjectives, 0, "DTLZ4");
+		setBoundsOnVariables();
+	}
+
+	/** Evaluate() method */
+	public void evaluate(Solution solution) {
+		int numberOfVariables = getNumVariables();
+		int numberOfObjectives = getNumObjectives();
+		double[] f = new double[numberOfObjectives];
+		double[] x = new double[numberOfVariables];
+		
+		double alpha = 100.0;
+
+		for (int i = 0; i < numberOfVariables; i++) {
+			x[i] = solution.getVariable(i);
+		}
+		
+		int k = getNumVariables() - getNumObjectives() + 1;
+
+		double g = 0.0;
+		for (int i = numberOfVariables - k; i < numberOfVariables; i++) {
+			g += (x[i] - 0.5) * (x[i] - 0.5);
+		}
+
+		for (int i = 0; i < numberOfObjectives; i++) {
+			f[i] = 1.0 + g;
+		}
+
+		for (int i = 0; i < numberOfObjectives; i++) {
+			for (int j = 0; j < numberOfObjectives - (i + 1); j++) {
+				f[i] *= java.lang.Math.cos(java.lang.Math.pow(x[j], alpha) * (java.lang.Math.PI / 2.0));
+			}
+			if (i != 0) {
+				int aux = numberOfObjectives - (i + 1);
+				f[i] *= java.lang.Math.sin(java.lang.Math.pow(x[aux], alpha) * (java.lang.Math.PI / 2.0));
+			}
+		}
+
+		for (int i = 0; i < numberOfObjectives; i++) {
+			solution.setObjective(i, f[i]);
+		}
+	}
+
+	@Override
+	public void evaluateConstraints(Solution solution) {
+		return;
+	}
+
+	@Override
+	public void setBoundsOnVariables() {
+		for (int i = 0; i < getNumVariables(); i++) {
+			setLowerBound(i, 0.0);
+			setUpperBound(i, 1.0);
+		}
+	}
+}
