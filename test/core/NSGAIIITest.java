848bd2d2013b1f4e4ab089963ae73197cb5a0890
Tomasz Sternal <sternal.tomasz@gmail.com>
2016-05-30 00:25:37 +0200

    Fix nsgaIII test- missing arguments in NSGAIII.run call

--- test/core/NSGAIIITest.java
+++ test/core/NSGAIIITest.java
@@ -9,7 +9,7 @@ import problems.DTLZ1;
 public class NSGAIIITest {
 	@Test
 	public void nsgaIIIinitialization(){
-		NSGAIII alg = new NSGAIII(new DTLZ1(3), 400, false);
+		NSGAIII alg = new NSGAIII(new DTLZ1(3), 400, false, 50);
 		
 		assertEquals("DTLZ1",alg.getProblem().getName());
 		assertEquals(92, alg.getPopulationSize());
