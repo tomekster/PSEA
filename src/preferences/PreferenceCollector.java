2009bc5c54409d87b80beec0722e5aef9227d9f8
tomekster <sternal.tomasz@gmail.com>
2017-05-05 17:53:00 +0200

    hotfix

--- src/preferences/PreferenceCollector.java
+++ src/preferences/PreferenceCollector.java
@@ -3,14 +3,13 @@ package preferences;
 import java.util.ArrayList;
 
 import core.points.Solution;
-import utils.RACS;
 
 public class PreferenceCollector {
 	
 	private ArrayList<Comparison> comparisonsList = new ArrayList<>();
 	
 	public void addComparison(Solution s1, Solution s2) {
-		comparisonsList.add(new Comparison(s1,s2, RACS.findMinEps(s1, s2), RACS.findMaxEps(s1, s2)));
+		comparisonsList.add(new Comparison(s1,s2, 0, 0));
 		
 	}
 	
